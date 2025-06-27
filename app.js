1c9ff9acca696c4029fcc55016ab6f37355d60c4
ErgoMundusDovi <eddylou91@gmail.com>
2025-06-27 13:00:26 +0200

    ...

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/year', routerYear)
+app.use('/', routerYear)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
