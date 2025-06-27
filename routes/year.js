f6bf72dc24ba21843beae3f119debd5137d3dee1
ErgoMundusDovi <eddylou91@gmail.com>
2025-06-27 13:11:06 +0200

    ...

--- routes/year.js
+++ routes/year.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res,)=>{
-    const date = new Date()
-    let year= date.getFullYear()
-  res.json(year);
+    
+  res.json({year: "2025"});
 });
 
 
