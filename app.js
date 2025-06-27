9fe6eb2754e8dba8c38c85cc91f042d996bfa114
ErgoMundusDovi <eddylou91@gmail.com>
2025-06-27 12:27:41 +0200

    ...

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var routerYear= require('./routes/year')
 var app = express();
-
+app.use(cors());
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/year', routerYear)
